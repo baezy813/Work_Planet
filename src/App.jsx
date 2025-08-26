0c22cd1519a263d258f30a4b6b14e4c1f00f14d2
juyeon-Bae <w2316@e-mirim.hs.kr>
2025-08-26 21:33:27 +0900

    feat: refactor App component and enhance routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,63 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import JoinForm from './pages/Login/join'
-import LoginForm from './pages/Login/login'
-import Mypage from "./pages/Mypage/MyPage"
+import { FavoriteProvider } from './contexts/FavoriteContext'
+import Navbar from './components/common/Navbar'
+import LogoutPopup from './components/common/LogoutPopup'
+import JoinForm from './pages/Auth/join'
+import LoginForm from './pages/Auth/login'
+import Mypage from './pages/Mypage/MyPage'
 import RandomQuote from './pages/Quote/RandomQuote'
+import MenuRecommendation from './pages/Restaurant/MenuRecommendation'
+import PlayList from './pages/PlayList/page'
+import styled from 'styled-components'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <main className="main-content">
-            <Routes>
-              <Route path="/home" element={<Home />} />
-              <Route path="/join" element={<JoinForm />} />
-              <Route path="/" element={<LoginForm />} />
-              <Route path="/mypage" element={<Mypage />} />
-              <Route path="/quote" element={<RandomQuote />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </AuthProvider>
-  )
+    const [isLogoutPopupOpen, setIsLogoutPopupOpen] = useState(false)
+
+    return (
+        <AuthProvider>
+            <FavoriteProvider>
+                <Router>
+                    <div className="App">
+                      
+                        <LogoutPopup 
+                            isLogoutPopupOpen={isLogoutPopupOpen} 
+                            setIsLogoutPopupOpen={setIsLogoutPopupOpen} 
+                        />
+                        
+                       
+                        <Routes>
+                            <Route path="/login" element={<LoginForm />} />
+                            <Route path="/join" element={<JoinForm />} />
+                            <Route path="*" element={
+                                <>
+                                    <Navbar setIsLogoutPopupOpen={setIsLogoutPopupOpen} />
+                                    <S.MainContent>
+                                        <Routes>
+                                            <Route path="/mypage" element={<Mypage />} />
+                                            <Route path="/quote" element={<RandomQuote />} />
+                                            <Route path="/restaurant" element={<MenuRecommendation />} />
+                                            <Route path="/playlist" element={<PlayList />} />
+                                        </Routes>
+                                    </S.MainContent>
+                                </>
+                            } />
+                        </Routes>
+                    </div>
+                </Router>
+            </FavoriteProvider>
+        </AuthProvider>
+    )
 }
 
 export default App
+
+const S = {
+    MainContent: styled.main`
+        margin-left: 280px; /* 사이드바 너비만큼 여백 */
+        min-height: 100vh;
+        background: #f8fafc;
+        padding: 20px;
+    `
+}
